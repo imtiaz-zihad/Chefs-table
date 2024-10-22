e5ddf6779b7d7d0e09af47b3608819d495d94d4c
Imtiaz-Zihad <mdimtiazzihad@gmail.com>
2024-10-22 17:42:09 +0600

    Enviroment Setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Banner from "./componentes/Banner"
+import Header from "./componentes/Header"
+import OurRecipe from "./componentes/OurRecipe"
+
+const App = () =>{
+  return <div>
+    <div className="text-center">
+    <Header></Header>
+    <Banner></Banner>
+    <OurRecipe></OurRecipe>
+    </div>
+
+    
+  </div>
+}
+
+export default App
