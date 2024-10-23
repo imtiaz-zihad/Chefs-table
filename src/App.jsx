ff0b459ab920faf683e979d347b54f497ef6b549
Imtiaz-Zihad <mdimtiazzihad@gmail.com>
2024-10-23 08:48:58 +0600

    Sidebar Added and data display

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,71 @@
-import { useState } from "react"
-import Banner from "./componentes/Banner"
-import Header from "./componentes/Header"
-import OurRecipe from "./componentes/OurRecipe"
-import Recipes from "./componentes/Recipes"
-import Sidebar from "./componentes/Sidebar"
+import { useState } from "react";
+import Banner from "./componentes/Banner";
+import Header from "./componentes/Header";
+import OurRecipe from "./componentes/OurRecipe";
+import Recipes from "./componentes/Recipes";
+import Sidebar from "./componentes/Sidebar";
 
-const App = () =>{
-
+const App = () => {
   const [recipeQueue, setRecipeQueue] = useState([]);
+  const [preparedRecipe, setPrepareRecipe] = useState([]);
+  const [totalTime, setTotalTime] = useState(0);
+  const [totalCalories, setTotalCalories] = useState(0);
 
-  const addRecipeQueue = (recipe) =>{
-
-    const isExists = recipeQueue.find(previousRecipe => previousRecipe.recipe_id === recipe.recipe_id)
+  const addRecipeQueue = (recipe) => {
+    const isExists = recipeQueue.find(
+      (previousRecipe) => previousRecipe.recipe_id === recipe.recipe_id
+    );
 
     if (!isExists) {
-      setRecipeQueue([...recipeQueue ,recipe])
-    }else{
-      alert('Recipe already added in the queue')
+      setRecipeQueue([...recipeQueue, recipe]);
+    } else {
+      alert("Recipe already added in the queue");
     }
+  };
+  const handleRemove = (id) => {
+    const deletedRecipes = recipeQueue.find(
+      (recipe) => recipe.recipe_id === id
+    );
 
-   
+
+    const UpdatedQueue = recipeQueue.filter(
+      (recipe) => recipe.recipe_id !== id
+    );
+    setRecipeQueue(UpdatedQueue);
+    setPrepareRecipe([...preparedRecipe, deletedRecipes]);
+  };
+
+  const calculateTimeAndCalories = (time, calorie) =>{
+    setTotalTime(totalTime + time)
+    setTotalCalories(totalCalories + calorie)
   }
-  console.log(recipeQueue);
-  
 
-  return <div>
-    <div className="text-center">
-    <Header></Header>
-    <Banner></Banner>
-    <OurRecipe></OurRecipe>
+  return (
+    <div>
+      <div className="text-center">
+        <Header></Header>
+        <Banner></Banner>
+        <OurRecipe></OurRecipe>
+      </div>
+
+      {/* Recipes Card Section  */}
+      <section className="flex flex-col md:flex-row gap-6  max-w-7xl mx-auto">
+        {/* Card Section */}
+        <Recipes addRecipeQueue={addRecipeQueue}></Recipes>
+        {/* Side Bar */}
+        <Sidebar
+        totalCalories={totalCalories}
+        totalTime={totalTime}
+        calculateTimeAndCalories={calculateTimeAndCalories}
+          handleRemove={handleRemove}
+          recipeQueue={recipeQueue}
+          preparedRecipe={preparedRecipe}
+        >
+          {" "}
+        </Sidebar>
+      </section>
     </div>
+  );
+};
 
-    {/* Recipes Card Section  */}
-    <section className="flex flex-col md:flex-row gap-6  max-w-7xl mx-auto">
-      {/* Card Section */}
-      <Recipes addRecipeQueue={addRecipeQueue}></Recipes>
-      {/* Side Bar */}
-      <Sidebar recipeQueue={recipeQueue}> </Sidebar>
-    </section>
-
-  </div>
-}
-
-export default App
+export default App;
--- src/componentes/Sidebar.jsx
+++ src/componentes/Sidebar.jsx
@@ -1,33 +1,82 @@
-const Sidebar = ({recipeQueue}) => {
+const Sidebar = ({ recipeQueue, handleRemove, preparedRecipe,calculateTimeAndCalories, totalCalories, totalTime }) => {
   return (
     <div className="md:w-1/3 border-2 rounded-2xl text-gray-600 p-2 bg-base-100">
       {/* Want to cook */}
       <div className="overflow-x-auto">
-        <h2 className="border-b-2 mx-auto mt-3 pb-4 font-semibold text-2xl text-gray-800 text-center">Want to cook: {recipeQueue.length}</h2>
-  <table className="table ">
-    {/* head */}
-    <thead >
-      <tr >
-        <th></th>
-        <th>Name</th>
-        <th>Time</th>
-        <th>Calories</th>
-        <th></th>
-      </tr>
-    </thead>
-    <tbody>
-      {recipeQueue.map((recipe,idx)=> <tr className="hover" key={idx}>
-        <th>{idx + 1}</th>
-        <td>{recipe.recipe_name}</td>
-        <td>{recipe.preparing_time}</td>
-        <td>{recipe.calories}</td>
-        <button className="btn bg-green-400  mt-5 px-2 py-1 m-2  rounded-3xl text-black border-none">Preparing</button>
-      </tr>)}
-      
-      
-    </tbody>
-  </table>
-</div>
+        <h2 className="border-b-2 mx-auto mt-3 pb-4 font-semibold text-2xl text-gray-800 text-center">
+          Want to cook: {recipeQueue.length}
+        </h2>
+        <table className="table ">
+          {/* head */}
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Time</th>
+              <th>Calories</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {recipeQueue.map((recipe, idx) => (
+              <tr className="hover" key={idx}>
+                <th>{idx + 1}</th>
+                <td>{recipe.recipe_name}</td>
+                <td>{recipe.preparing_time}</td>
+                <td>{recipe.calories}</td>
+                <button
+                  onClick={() => {
+                    
+                  
+                    handleRemove(recipe.recipe_id)
+                    calculateTimeAndCalories(recipe.preparing_time,recipe.calories)
+                  
+                  }}
+                  className="btn bg-green-400  mt-5 px-2 py-1 m-2  rounded-3xl text-black border-none"
+                >
+                  Preparing
+                </button>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      {/* Currently cook */}
+      <div className="overflow-x-auto mt-8">
+        <h2 className="border-b-2 mx-auto mt-3 pb-4 font-semibold text-2xl text-gray-800 text-center">
+          Currently Cooking: {preparedRecipe.length}
+        </h2>
+        <table className="table ">
+          {/* head */}
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Time</th>
+              <th>Calories</th>
+              
+            </tr>
+          </thead>
+          <tbody className="border-b-2">
+            {preparedRecipe.map((recipe, idx) => (
+              <tr className="hover" key={idx}>
+                <th>{idx + 1}</th>
+                <td>{recipe.recipe_name}</td>
+                <td>{recipe.preparing_time}</td>
+                <td>{recipe.calories}</td>
+                
+              </tr>
+              
+            ))}
+            <tr>
+            <th>{}</th>
+                <td></td>
+                <td>Total time ={totalTime}</td>
+                <td>Total Calories={totalCalories}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
